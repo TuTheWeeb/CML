caee64d1dd2801318075a09fb80aff2a475ea475
TuTheWeeb <eduardo.tuler@gmail.com>
2026-03-23 01:38:06 -0300

    dot product and rand corrected

--- src/main.c
+++ src/main.c
@@ -46,25 +46,24 @@ TYPE_LIST(X)
 
 #define DefineRand(T) \
   void rand_##T(T##Array array, u32 seed, u64 max_value) { \
-    _Pragma("omp parallel private(seed)") \
+    _Pragma("omp parallel") \
     { \
     u32 my_seed = seed * omp_get_thread_num(); \
-    _Pragma("omp parallel for") \
+    _Pragma("omp for") \
     for (size_t i = 0; i < array.size; i++) array.data[i] = rand_r(&my_seed) % max_value; \
     } \
   }
 
 #define DefineSum(T) \
-  T##Array * sum_##T(T##Array array1, T##Array array2) { \
+  T##Array sum_##T(T##Array array1, T##Array array2) { \
     if (array1.size != array2.size) { \
       printf("The arrays have different sizes (%ld != %ld), returning error! ", array1.size, array2.size); \
-      return NULL; \
+      return (T##Array) {.data=NULL, .size=0}; \
     } \
-    T##Array * ret = malloc(sizeof(T##Array) * 1); \
-    *ret = Array(T, array1.size); \
+    T##Array ret = Array(T, array1.size); \
     _Pragma("omp parallel for") \
       for (size_t i = 0; i < array1.size; i++) { \
-        ret->data[i] = array1.data[i] + array2.data[i]; \
+        ret.data[i] = array1.data[i] + array2.data[i]; \
       } \
     return ret; \
   }
@@ -78,26 +77,28 @@ TYPE_LIST(X)
   }
 
 #define DefineDotProduct(T) \
-    T##Array * dot_product_##T(T##Array array1, T##Array array2) { \
+    T##Array dot_product_##T(T##Array array1, T##Array array2) { \
       if (array1.size != array2.size) {\
         printf("The arrays have different sizes (%ld != %ld), returning error! ", array1.size, array2.size); \
-        return NULL; \
+        return (T##Array) {.data=NULL, .size=0}; \
       } \
-      T##Array * ret = malloc(sizeof(T##Array) * 1); \
-      *ret = Array(T, array1.size); \
+      T##Array ret = Array(T, array1.size); \
       _Pragma("omp parallel for") \
       for (size_t i = 0; i < array1.size; i++) {\
-        ret->data[i] = array1.data[i] * array2.data[i]; \
+        ret.data[i] = array1.data[i] * array2.data[i]; \
       } \
       return ret; \
     }
 
 #define DefinePrint(T, format) \
   void print_##T##Array(T##Array array, size_t limit) { \
+    if (limit >= array.size) { \
+      limit = array.size; \
+    } \
     printf("{"); \
     for (size_t i = 0; i < limit; i++) { \
         printf(format, array.data[i]); \
-        if (i != array.size - 1) \
+        if (i != limit - 1) \
         printf(", "); \
     } \
     printf("}\n"); \
@@ -111,6 +112,7 @@ TYPE_LIST(X)
   DefineOnes(alias) \
   DefineSum(alias) \
   DefineScalarMUL(alias) \
+  DefineDotProduct(alias) \
   DefinePrint(alias, format)
 TYPE_LIST(GENERATE_ALL)
 
@@ -120,7 +122,7 @@ TYPE_LIST(GENERATE_ALL)
 #define DISPATCH_SUM(real_type, alias, format) alias##Array: sum_##alias,
 #define DISPATCH_PRINT(real_type, alias, format) alias##Array: print_##alias##Array,
 #define DISPATCH_SCALAR_MUL(real_type, alias, format) alias##Array: scalar_mul_##alias,
-#define DISPATCH_DOT(real_type, alias, format) alias##Array: ##alias, dot_product_##alias,
+#define DISPATCH_DOT(real_type, alias, format) alias##Array: dot_product_##alias,
 
 #define cml_zeros(arr) _Generic((arr), TYPE_LIST(DISPATCH_ZEROS) default: NULL)(arr)
 #define cml_ones(arr) _Generic((arr), TYPE_LIST(DISPATCH_ONES) default: NULL)(arr)
@@ -131,18 +133,15 @@ TYPE_LIST(GENERATE_ALL)
 #define cml_dot(arr1, arr2) _Generic((arr1), TYPE_LIST(DISPATCH_DOT) default: NULL)(arr1, arr2)
 
 int main() {
-  
-  i8Array array = Array(i8, 10);
-  i8Array array2 = Array(i8, 10);
-  u64 scalar = 2;
+  size_t size = 10;
+  i8Array array = Array(i8, size);
+  i8Array array2 = Array(i8, size);
 
-  if (array.data != NULL) {
+  if (array.data != NULL || array2.data != NULL) {
     cml_rand(array, 42, 5);
-    cml_rand(array2, 91, 2);
-    cml_print(array, array.size);
-    cml_print(array2, array.size);
-    i8Array * res = cml_dot(array, array2);
-    cml_print(ret, 10);
+    cml_rand(array2, 97, 6);
+    i8Array res = cml_dot(array, array2);
+    cml_print(res, res.size);
   }
 
   return 0;
