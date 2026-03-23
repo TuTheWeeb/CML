8a4e5e0c71cb73cb60d781c3a5699a3c7014dd66
TuTheWeeb <eduardo.tuler@gmail.com>
2026-03-23 16:28:42 -0300

    Added comments

--- src/main.c
+++ src/main.c
@@ -19,33 +19,39 @@
 #define X(real_type, alias, printformat) typedef real_type alias;
 TYPE_LIST(X)
 
+// Declare all types of Array
 #define DeclareArray(T) typedef struct { \
   T * restrict data; \
   size_t size; \
 } T##Array;
 
-
+// Automatically alocates memory
 #define Array(T, sz) (T##Array) {.data = (T*)malloc(sizeof(T) * (sz)), .size=(sz)}
 
+// For manual use
 #define FreeArray(array) \
     free(array.data);
 
+// For manual use
 #define FreeArrayPointer(array) \
   FreeArray(*array); \
   free(array);
 
+// Defines all zeros functions
 #define DefineZeros(T) \
   void zeros_##T(T##Array array) { \
     _Pragma("omp parallel for") \
     for (size_t i = 0; i < array.size; i++) array.data[i] = 0; \
   }
 
+// Defines all ones functions
 #define DefineOnes(T) \
   void ones_##T(T##Array array) { \
     _Pragma("omp parallel for") \
     for (size_t i = 0; i < array.size; i++) array.data[i] = 1; \
   }
 
+// Defines all rands functions
 #define DefineRand(T) \
   void rand_##T(T##Array array, u32 seed, u64 max_value) { \
     _Pragma("omp parallel") \
@@ -56,6 +62,7 @@ TYPE_LIST(X)
     } \
   }
 
+// Defines all sum functions
 #define DefineSum(T) \
   T##Array sum_##T(T##Array array1, T##Array array2) { \
     if (array1.size != array2.size) { \
@@ -70,6 +77,7 @@ TYPE_LIST(X)
     return ret; \
   }
 
+// Defines all scalar functions
 #define DefineScalarMUL(T) \
   void scalar_mul_##T(T##Array array, u64 scalar) { \
     _Pragma("omp parallel for") \
@@ -78,6 +86,7 @@ TYPE_LIST(X)
       } \
   }
 
+// Defines all dot product funcions
 #define DefineDotProduct(T) \
     T dot_product_##T(T##Array array1, T##Array array2) { \
       if (array1.size != array2.size) {\
@@ -92,6 +101,7 @@ TYPE_LIST(X)
       return acum; \
     }
 
+// Defines all map functions
 #define DefineMap(T) \
     T##Array map_##T##Array(T##Array array, T (*func)(T)) { \
       if (array.data == NULL) { \
@@ -106,6 +116,7 @@ TYPE_LIST(X)
       return ret; \
     }
 
+// Defines all print functions
 #define DefinePrint(T, format) \
   void print_##T##Array(T##Array array, size_t limit) { \
     if (limit >= array.size) { \
@@ -120,12 +131,14 @@ TYPE_LIST(X)
     printf("}\n"); \
   }
 
+// Define all array clean functions
 #define DefineArrayFree(T) \
     void free_##T##Array(T##Array * array) {\
       if (array->data != NULL) free(array->data); \
       array->data = NULL; \
     }
 
+// Expand each macro
 #define GENERATE_ALL(real_type, alias, format) \
   DeclareArray(alias) \
   DefineZeros(alias) \
@@ -139,6 +152,7 @@ TYPE_LIST(X)
   DefinePrint(alias, format)
 TYPE_LIST(GENERATE_ALL)
 
+// Creates DISPATCHES for all functions
 #define DISPATCH_ZEROS(real_type, alias, format) alias##Array: zeros_##alias,
 #define DISPATCH_ONES(real_type, alias, format) alias##Array: ones_##alias,
 #define DISPATCH_RAND(real_type, alias, format) alias##Array: rand_##alias,
@@ -148,6 +162,7 @@ TYPE_LIST(GENERATE_ALL)
 #define DISPATCH_DOT(real_type, alias, format) alias##Array: dot_product_##alias,
 #define DISPATCH_MAP(real_type, alias, format) alias##Array: map_##alias##Array,
 
+// Use the DISPATCHES to create generics
 #define cml_zeros(arr) _Generic((arr), TYPE_LIST(DISPATCH_ZEROS) default: NULL)(arr)
 #define cml_ones(arr) _Generic((arr), TYPE_LIST(DISPATCH_ONES) default: NULL)(arr)
 #define cml_rand(arr, s, max) _Generic((arr), TYPE_LIST(DISPATCH_RAND) default: NULL)(arr, s, max)
@@ -167,14 +182,12 @@ int main() {
   ArrayInit(array, i8, size);
   ArrayInit(array1, i8, size);
 
-  if (array.data != NULL) {
-    cml_rand(array, 42, 256);
-    cml_rand(array1, 41, 256);
-    cml_print(array);
-    cml_print(array1);
-    i8 res = cml_dot(array, array1);
-    printf("res: %d\n", res);
-  }
+  cml_rand(array, 42, 256);
+  cml_rand(array1, 41, 256);
+  cml_print(array);
+  cml_print(array1);
+  i8 res = cml_dot(array, array1);
+  printf("res: %d\n", res);
 
   cml_print(array);
   cml_print(array1);
